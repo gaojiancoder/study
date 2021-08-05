e2eeaed365a45c231d6907f703833ed24e916458
gaojian <gaojian_lisp@outlook.com>
2021-08-05 20:46:45 +0800

    解决tomcat8以上post中文乱码

--- servlet/src/main/java/io/gaojian/servlet/TestServlet.java
+++ servlet/src/main/java/io/gaojian/servlet/TestServlet.java
@@ -9,24 +9,34 @@ import java.io.IOException;
 public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        StringBuffer url = request.getRequestURL();
-        System.out.println("获取全部路径"+url);
-        String uri = request.getRequestURI();
-        System.out.println("获取部分路径"+uri);
-        String queryString = request.getQueryString();
-        System.out.println("请求时参数字符串"+queryString);
-        String method = request.getMethod();
-        System.out.println("请求方式"+method);
-        String protocol = request.getProtocol();
-        System.out.println("协议版本"+protocol);
-        String path = request.getContextPath();
-        System.out.println("项目站点名"+path);
-        String parameter = request.getParameter("uname");
-        System.out.println(""+parameter);
+//        StringBuffer url = request.getRequestURL();
+//        System.out.println("获取全部路径"+url);
+//        String uri = request.getRequestURI();
+//        System.out.println("获取部分路径"+uri);
+//        String queryString = request.getQueryString();
+//        System.out.println("请求时参数字符串"+queryString);
+//        String method = request.getMethod();
+//        System.out.println("请求方式"+method);
+//        String protocol = request.getProtocol();
+//        System.out.println("协议版本"+protocol);
+//        String path = request.getContextPath();
+//        System.out.println("项目站点名"+path);
+
+        String uname = request.getParameter("uname");
+        String upwd = request.getParameter("upwd");
+        System.out.println("uname :"+uname);
+        System.out.println("upwd :"+upwd);
+
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.setCharacterEncoding("UTF-8");
+        String uname = request.getParameter("uname");
+        String upwd = request.getParameter("upwd");
+        System.out.println("uname :"+uname);
+        System.out.println("upwd :"+upwd);
     }
 }
