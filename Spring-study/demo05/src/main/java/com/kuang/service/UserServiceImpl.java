b927e7fd97cf447d1c59f8f7de547f6963e1eee1
gaojian <gaojian_lisp@outlook.com>
2021-08-12 20:58:12 +0800

    mybatis

--- Spring-study/demo05/src/main/java/com/kuang/service/UserServiceImpl.java
+++ Spring-study/demo05/src/main/java/com/kuang/service/UserServiceImpl.java
@@ -1,4 +1,24 @@
 package com.kuang.service;
 
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+
+    @Override
+    public void add() {
+        System.out.println("增加了用户");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("删除了用户");
+    }
+
+    @Override
+    public void update() {
+        System.out.println("改了用户");
+    }
+
+    @Override
+    public void select() {
+        System.out.println("查了用户");
+    }
 }
