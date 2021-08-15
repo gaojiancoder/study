d7bee7d847dde20627f6f6742d0577cdbad93d42
gaojian <gaojian_lisp@outlook.com>
2021-08-15 13:45:34 +0800

    练习不重要

--- maven/hello/module1/src/main/java/org/example/App.java
+++ maven/hello/module1/src/main/java/org/example/App.java
@@ -1,13 +1,28 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args){
+        test t = new test();
+        t.map();
+
     }
+
+    static class test{
+        private Integer[] array = {10,3,3,15,9,23};
+
+        private void map(){
+            List<Integer> collect = Stream.of(array).map(n -> n*2).collect(Collectors.toList());
+            System.out.println(collect);
+        }
+    }
+
+
+
+
+
 }
